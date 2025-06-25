0f3e9228229658b8ad4043e87a1f0b31d9d149ff
Naman Kaushik <naman.kaushik@impactqa.com>
2025-06-25 11:09:13 +0530

    logging commit

--- api_keys.py
+++ api_keys.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 # Hardcoded API Key for demonstration
 VALID_API_KEY = os.getenv("Key")
+logging.info(f"Importing Token frim env")
 
 class Middleware:
     def __init__(self, app):
@@ -20,10 +21,14 @@ class Middleware:
         if not api_key or api_key != VALID_API_KEY:
             res = Response("Unauthorized: Invalid or missing API key", mimetype='text/plain', status=401)
             return res(environ, start_response)
+        logging.error("Unaurthorized : invalid or missing key , check the key or input")
 
         # Store user info in request environment
         environ["user"] = {"name": "Authorized User"}
         return self.app(environ, start_response)
+logging.info*("stroing user info into the environment")
+
+logging.info("Implementing middleware in the API")
 
 app = Flask(__name__)
 
@@ -34,16 +39,19 @@ app.wsgi_app = Middleware(app.wsgi_app)
 def index():
     user = request.environ.get("user")
     return jsonify(message=f"Welcome, {user['name']}! Your API Key is valid.")
+logging.info(f"Requesting basic endpoint")
 
 @app.route("/hello", methods=["GET"])
 def hello():
     user = request.environ.get("user")
     return jsonify(message=f"Hello, {user['name']}! Hope you're doing great!")
+logging.info(f"Requesting /hello endpoint")
 
 @app.route("/goodbye", methods=["GET"])
 def goodbye():
     user = request.environ.get("user")
     return jsonify(message=f"Goodbye, {user['name']}! Take care!")
+logging.info(f"Requesting /goodbye endpoint")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
